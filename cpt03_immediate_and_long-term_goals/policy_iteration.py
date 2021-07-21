0d72ffe2634c7e33a4cbd7835eb5580354575a35
Тарасов Денис <dt6cgsg@gmail.com>
2021-07-22 01:14:34 +0300

    Chapter 3. Policy and value iterations

---
+++ cpt03_immediate_and_long-term_goals/policy_iteration.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from policy_evaluation import policy_evaluation
+from policy_improvement import policy_improvement
+
+
+def policy_iteration(P, gamma=1.0, theta=1e-10):
+    random_actions = np.random.choice(tuple(P[0].keys()), len(P))
+    pi = lambda s: {s:a for s, a in enumerate(random_actions)}[s]
+
+    while True:
+        old_pi = {s:pi(s) for s in range(len(P))}
+
+        V = policy_evaluation(pi, P, gamma, theta)
+
+        pi = policy_improvement(V, P, gamma)
+
+        if old_pi == {s: pi(s) for s in range(len(P))}:
+            break
+    return V, pi
